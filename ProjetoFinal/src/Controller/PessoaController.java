5e456d53a4a4416ac2217b50034bdcf4d5f5b3d7
lucas-vrm <luccas_menezzes@hotmail.com>
2023-11-20 19:40:58 -0300

    MVC classe pessoa (teste)

--- ProjetoFinal/src/Controller/PessoaController.java
+++ ProjetoFinal/src/Controller/PessoaController.java
@@ -1,5 +1,23 @@
 package Controller;
 
-public class PessoaController {
+import Model.Pessoa;
+import Model.Pessoa.TipoMembro;
+import View.PessoaView;
 
+public class PessoaController {
+	private Pessoa model;
+	private PessoaView view;
+	
+	public PessoaController(Pessoa model, PessoaView view) {
+		this.model = model;
+		this.view = view;
+		
+	}
+	
+	public void setDetalhesDaPessoa(String nome, String contato, int id, TipoMembro tipoDeMembro) {
+		model.setNome(nome);
+		model.setContato(contato);
+		model.setId(id);
+		model.setTipoDeMembro(tipoDeMembro);
+	}
 }
