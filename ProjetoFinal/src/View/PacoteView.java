55c39400f66d9b31dedec307103b0c24407d9e59
Ana Carolina de Almeida Cardoso <ra246914@barney.lab.ic.unicamp.br>
2023-11-25 15:45:49 -0300

    adicionei algumas informações a serem exibidas sobre um pacote de viagem a partir da classe PacoteView

--- ProjetoFinal/src/View/PacoteView.java
+++ ProjetoFinal/src/View/PacoteView.java
@@ -1,5 +1,16 @@
 package View;
+import Model.Pacote;
+import Model.Destino;
+import Model.CategoriaDeDestino;
 
 public class PacoteView {
-
+    // Vamos exibir algumas informações simples sobre um determinado pacote de viagem
+    public static void exibirInformacoesPacote(Pacote pacote){
+        System.out.println("Informações do Pacote:");
+        System.out.println("Destino: " + pacote.getDestino().getNome());
+        System.out.println("Duração: " + pacote.getDuracao() + " dias");
+        System.out.println("Preço: R$ " + pacote.getPreco());
+        System.out.println("Assentos disponíveis: " + pacote.getAssentosDisponiveis());
+        System.out.println("Categoria: " + pacote.getCategoriaDeDestino().getDescricao());
+    }
 }
