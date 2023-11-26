6c4d73914b3c47900b05f78cec4545fa0daa8f2b
ANA CAROLINA DE ALMEIDA CARDOSO <anacarolinadealmeidacardoso@MacBook-Air-de-ANA.local>
2023-11-26 16:57:52 -0300

    coloquei as informacoes da classe Destino para serem exibibidas por uma classe DestinoView

--- ProjetoFinal/src/View/DestinoView.java
+++ ProjetoFinal/src/View/DestinoView.java
@@ -1,5 +1,15 @@
 package View;
 
-public class DestinoView {
+import Model.Destino;
 
+public class DestinoView {
+    public static void exibirInformacoesDestino(Destino destino){
+        System.out.println("Informações do Destino:");
+        System.out.println("Nome do destino: " + destino.getNome());
+        System.out.println("Descrição do destino: " + destino.getDescricao());
+        System.out.println("Principais pontos turísticos: ");
+        for (String pontoTuristico : destino.getPontosTuristicos()) {
+            System.out.println(pontoTuristico);
+        }
+    }
 }
