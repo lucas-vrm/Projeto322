bb4902665ca4fda8b27cda4dd81e2064c5803bf0
ANA CAROLINA DE ALMEIDA CARDOSO <anacarolinadealmeidacardoso@MacBook-Air-de-ANA.local>
2023-11-26 16:40:10 -0300

    Fiz alguns ajustes referentes a classe do Cliente.

--- ProjetoFinal/src/Model/Cliente.java
+++ ProjetoFinal/src/Model/Cliente.java
@@ -1,24 +1,29 @@
 package Model;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.Set;
+import java.util.HashSet;
 
 public class Cliente extends Usuario {
 	
-	private ArrayList<Destino> destinos;
+	private ArrayList<Pacote> pacotesComprados;
 	private double creditos;
 	
 	public Cliente(String nome, String contato, String email, String senha, int id) {
 		super(nome, contato, email, senha, id);
-		this.destinos = new ArrayList<Destino>();
+		this.pacotesComprados = new ArrayList<Pacote>();
 		this.creditos = 0.0;
 	}
 
-	public ArrayList<Destino> getDestinos() {
-		return destinos;
+	public ArrayList<Pacote> getPacotesComprados() {
+		return pacotesComprados;
 	}
 
-	public void setDestinos(ArrayList<Destino> destinos) {
-		this.destinos = destinos;
+	public void setPacotesComprados(ArrayList<Pacote> pacotesComprados) {
+		this.pacotesComprados = pacotesComprados;
 	}
 
 	public double getCreditos() {
@@ -27,6 +32,41 @@ public class Cliente extends Usuario {
 
 	public void setCreditos(double creditos) {
 		this.creditos = creditos;
+	}
+
+	// Método para adicionar um pacote de viagem comprado pelo cliente.
+	public void comprarPacoteViagem(Pacote pacote, int quantidadeAssentos){
+		if (creditos >= pacote.getPreco()){
+			// Para checarmos se ainda temos assentos disponíveis
+			boolean reservou = pacote.reservar(quantidadeAssentos);
+			if (reservou){
+				System.out.println("O pacote de viagem foi comprado com sucesso!");
+				pacotesComprados.add(pacote);
+				double creditoAtualizado = creditos - pacote.getPreco();
+				this.creditos = creditoAtualizado;
+			}
+		}
+		else {
+			System.out.println("Você não possui créditos suficientes para comprar este pacote.");
+		}
+	}
+
+	// Método para visualizarmos os pacotes de viagem por data
+	public List<Pacote> visualizarPorData(){
+		return pacotesComprados.stream()
+			.sorted(Comparator.comparing(Pacote::getDataPartida))
+			.collect(Collectors.toList());
+	}
+
+	// Método para visualizarmos os pacotes de viagem por destino
+	public Set<Destino> visualizarDestinosPacotesComprados(){
+		Set<Destino> destinos = new HashSet<>();
+
+		for (Pacote pacote : pacotesComprados) {
+			destinos.add(pacote.getDestino());
+		}
+
+		return destinos;
 	}
 	
 }
--- ProjetoFinal/src/View/PacoteView.java
+++ ProjetoFinal/src/View/PacoteView.java
@@ -1,5 +1,13 @@
 package View;
+import Model.Pacote;
 
 public class PacoteView {
-
+    // Vamos exibir algumas informações simples sobre um determinado pacote de viagem
+    public static void exibirInformacoesPacote(Pacote pacote){
+        System.out.println("Informações do Pacote:");
+        System.out.println("Destino: " + pacote.getDestino().getNome());
+        System.out.println("Duração: " + pacote.getDuracao() + " dias");
+        System.out.println("Preço: R$ " + pacote.getPreco());
+        System.out.println("Assentos disponíveis: " + pacote.getAssentosDisponiveis());
+    }
 }
