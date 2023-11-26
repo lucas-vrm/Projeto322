2172ddc45f6f8b46972b587da465fcae503936bc
lucas-vrm <luccas_menezzes@hotmail.com>
2023-11-26 19:04:24 -0300

    tarefas do adm, dessa vez branch feita a partir da main

---
+++ ProjetoFinal/src/Controller/AdministradorController.java
@@ -0,0 +1,23 @@
+package Controller;
+
+import Model.Administrador;
+import View.AdministradorView;
+import java.util.Scanner;
+
+public class AdministradorController {
+	private Administrador model;
+	private AdministradorView view;
+	private Scanner scanner;
+	
+	
+	AdministradorController(Administrador model, AdministradorView view) {
+		this.model = model;
+		this.view = view;
+		this.scanner = new Scanner(System.in);
+	}
+	
+	public void getUserInput() {
+        int userInput = scanner.nextInt();
+        model.fazAcao(userInput);
+    }
+}
---
+++ ProjetoFinal/src/View/AdministradorView.java
@@ -0,0 +1,14 @@
+package View;
+
+public class AdministradorView {
+	
+	private void printTasks() {
+		System.out.println("O que voce gostaria de fazer? (Digite de 0 a 4):");
+		System.out.println("1 - Criar novo Pacote de Viagens no sistema");
+		System.out.println("2 - Apagar Pacote de Viagens já existente no sistema");
+		System.out.println("3 - Apagar um conta de um Cliente já existente no sistema");
+		System.out.println("0 - Sair");
+	}
+	
+	
+}
