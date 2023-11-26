61e2ea78c84157b722187a824d66a9bda5aaa002
lucas-vrm <luccas_menezzes@hotmail.com>
2023-11-26 18:39:16 -0300

    correcoes na classe cliente para adequar com a main

--- ProjetoFinal/src/Model/Cliente.java
+++ ProjetoFinal/src/Model/Cliente.java
@@ -1,15 +1,21 @@
 package Model;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.Set;
+import java.util.HashSet;
+
 
 public class Cliente extends Usuario {
 	
-	private ArrayList<Destino> reservas;
+	private ArrayList<Pacote> pacotesComprados;
 	private double creditos;
 	
 	public Cliente(String nome, String contato, String email, int id) {
 		super(nome, contato, email, id);
-		this.reservas = new ArrayList<>();
+		this.pacotesComprados = new ArrayList<Pacote>();
 		this.creditos = 0.0;
 	}
 	
