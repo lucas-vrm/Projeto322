2528a16b2a11f9675df9050cc042b34465f44278
Lucas F S Oliveira <l260577@dac.unicamp.br>
2023-11-26 16:50:07 -0300

    login bug fix

--- ProjetoFinal/src/Main/Main.java
+++ ProjetoFinal/src/Main/Main.java
@@ -1,36 +1,16 @@
 package Main;
 
-import Model.Administrador;
-import Model.Cliente;
 import Model.Login;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// Login telaDeLogin = new Login();
-		Object user = Login.loginScreen(args);
+		Object usuario = Login.loginScreen(args);
+		MainMethods.controleDeTelas(usuario);
 		
-		if (user != null) {
-			String tipoDeUsuario = MainMethods.identificarUsuario(user);
-			switch (tipoDeUsuario) {
-				case "Client":
-					Cliente cliente = (Cliente) user;
-					System.out.println("---- Tela Cliente " + cliente.getNome() + " ----");
-					// Chamar tela do Cliente 
-				case "Administrador":
-					Administrador admin = (Administrador) user;
-					System.out.println("----- Tela Admin " + admin.getNome() + " -----");
-					// Chamar tela do Admin 
-				default:
-					return;
-			}
-		} else {
-			return;
-		}
+
 	}
-
-
-	
 
 
 
