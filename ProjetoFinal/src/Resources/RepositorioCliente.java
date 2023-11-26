f86d06636a562e3c10366024416b2ea78feddb88
Lucas F S Oliveira <l260577@dac.unicamp.br>
2023-11-26 18:20:12 -0300

    repositorio client fix

--- ProjetoFinal/src/Resources/RepositorioCliente.java
+++ ProjetoFinal/src/Resources/RepositorioCliente.java
@@ -7,7 +7,7 @@ import Model.Cliente;
 public class RepositorioCliente extends Repositorio {
     private static Driver clientDriver = new Driver();
     private static String[] clientHeaderNames = new String[]{"nome", "contato", "email", "senha", "id",
-            "dataDeRegistro", "destinos", "creditos"};
+            "dataDeRegistro", "pacotesComprados", "creditos"};
     private static String diretorioCliente = "registro_cliente.csv";
 
     public RepositorioCliente() {
